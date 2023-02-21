d0d5fc11e47d2c9e52dc9844c5e1a6d891fc016e
rgryder <125736648+rgryder@users.noreply.github.com>
2023-02-20 19:34:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -177,7 +177,7 @@ resource "aws_cloudfront_distribution" "crc" {
   }
   
   viewer_certificate {
-    acm_certificate_arn = data.aws_acm_certificate_validation.resume.arn
+    acm_certificate_arn = aws_acm_certificate_validation.resume.arn
     ssl_support_method = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
